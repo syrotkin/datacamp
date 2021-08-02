815848c6d58b3a950833969e3f865104a82e59b0
syrotkin <syrotkin@gmail.com>
2021-08-02 22:13:40 +0200

    end of case study

---
+++ 02-StatisticalThinkingInPython-2/datacamp2.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n + 1) / n
+
+    return x, y
+
+# Bootstrap replicate calculation function
+# func could be 'mean', 'median', etc.
+def bootstrap_replicate_1d(data, func):
+    """Generate bootstrap replicate of 1D data."""
+    bs_sample = np.random.choice(data, size=len(data))
+
+    return func(bs_sample)
+
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+
+    return bs_replicates
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+
+    return bs_slope_reps, bs_intercept_reps
+
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
+
+# Pearson correlation coefficient from Part 1
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    corr_mat = np.corrcoef(x, y)
+    return corr_mat[0,1]
+
+
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x, bs_y)
+
+    return bs_replicates
+
+
+def say_hello(value):
+    print("hello,   ", value)
+
+if __name__ == '__main__':
+    draw_bs_pairs([1], [2], pearson_r, size=2)
+    #say_hello("world1")
